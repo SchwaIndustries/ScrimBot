c2b7f07e464da90038c9a05da5c70b6609c448ff
Ian Reese <kalissaac@protonmail.com>
2020-06-11 10:29:28 -0700

    Remove unnecessary package

--- src/services/heroku.js
+++ src/services/heroku.js
@@ -1,19 +1,15 @@
-const https = require('https')
+const http = require('http')
 
 module.exports = exports = {
   name: 'heroku',
   enabled: true,
   process: async (GLOBALS) => {
-    GLOBALS.app.set('port', (process.env.PORT || 12500))
-
-    GLOBALS.app.get('/', function (request, response) {
-      response.send('Hey')
-    }).listen(GLOBALS.app.get('port'), function () {
-      console.log(`ScrimBot Initialized | Running on port ${GLOBALS.app.get('port')}`)
-    })
-
+    http.createServer((req, res) => {
+      res.writeHead(200)
+      res.end('This is ScrimBot, invite it to your Discord server here: https://discord.com/oauth2/authorize?client_id=715030981894995998&scope=bot&permissions=8')
+    }).listen(process.env.PORT || 12500)
     setInterval(() => {
-      https.get('https://valorant-scrim-bot.herokuapp.com')
+      http.get('http://valorant-scrim-bot.herokuapp.com')
     }, 5 * 60 * 1000) // 5 minutes in milliseconds
   }
 }
