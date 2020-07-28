a714ee44a44b4c1aef3ef3ad2930e6a8f3fb8d3b
Ian Reese <kalissaac@protonmail.com>
2020-07-27 20:32:18 -0700

    Automatically give users match notifications role

---
+++ src/services/matchNotifications.js
@@ -0,0 +1,11 @@
+module.exports = exports = {
+  name: 'matchNotifications',
+  enabled: true,
+  process: async (GLOBALS) => {
+    GLOBALS.client.on('guildMemberAdd', member => {
+      const guildData = await GLOBALS.db.collection('guilds').doc(member.guild.id).get()
+      if (!guildData.exists) return
+      member.roles.add(guildData.get('notificationRole'))
+    })
+  }
+}
