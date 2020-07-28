2f115761d9f1790b9b71a5029793addc5d16cf7c
Kalissaac <kalissaac@protonmail.com>
2020-07-27 20:58:41 -0700

    Make matchNotifications async

--- src/services/matchNotifications.js
+++ src/services/matchNotifications.js
@@ -2,7 +2,7 @@ module.exports = exports = {
   name: 'matchNotifications',
   enabled: true,
   process: async (GLOBALS) => {
-    GLOBALS.client.on('guildMemberAdd', member => {
+    GLOBALS.client.on('guildMemberAdd', async member => {
       const guildData = await GLOBALS.db.collection('guilds').doc(member.guild.id).get()
       if (!guildData.exists) return
       member.roles.add(guildData.get('notificationRole'))
