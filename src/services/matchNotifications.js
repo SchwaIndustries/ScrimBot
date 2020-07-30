108e592bb03c749705ab3ffa3039ac26a2d1062d
Kalissaac <kalissaac@protonmail.com>
2020-07-29 20:18:52 -0700

    Fix match notifications role giving

--- src/services/matchNotifications.js
+++ src/services/matchNotifications.js
@@ -5,7 +5,11 @@ module.exports = exports = {
     GLOBALS.client.on('guildMemberAdd', async member => {
       const guildData = await GLOBALS.db.collection('guilds').doc(member.guild.id).get()
       if (!guildData.exists) return
-      member.roles.add(guildData.get('notificationRole'))
+
+      const memberData = await GLOBALS.db.collection('users').doc(member.id).get()
+      if (memberData.exists && memberData.get('notifications')) {
+        member.roles.add(guildData.get('notificationRole'))
+      }
     })
   }
 }
