4f2cc0f199aad10716b8640829a0732dff13a822
Kalissaac <kalissaac@protonmail.com>
2020-12-24 13:43:10 -0800

    Fix indentation

--- src/commands/support.js
+++ src/commands/support.js
@@ -8,10 +8,10 @@ module.exports = exports = {
    */
   process: async (message, GLOBALS) => {
     const embed = new GLOBALS.Embed()
-    .setTitle('Support Server')
-    .setDescription('For support, please join [Valorant Collective](https://discord.gg/hfFJxUG), and head to the Scrimbot Support Channel!')
-    .setURL('https://discord.gg/hfFJxUG')
+      .setTitle('Support Server')
+      .setDescription('For support, please join [Valorant Collective](https://discord.gg/hfFJxUG), and head to the Scrimbot Support Channel!')
+      .setURL('https://discord.gg/hfFJxUG')
     message.channel.send(embed)
   }
-    
+
 }
