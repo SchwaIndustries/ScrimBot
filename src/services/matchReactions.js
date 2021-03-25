e20ab398821c2c59c2c1fcd751d3698f8ec03171
Kalissaac <kalissaac@protonmail.com>
2021-03-24 19:45:39 -0700

    Use update queries instead of document replacement

--- src/services/matchReactions.js
+++ src/services/matchReactions.js
@@ -56,10 +56,13 @@ const _addPlayerToMatch = async (reaction, user, GLOBALS, matchInformation) => {
 
   const messageEmbed = reaction.message.embeds[0]
 
+  const matchUpdateQuery = { $push: {} }
+
   switch (reaction.emoji.name) {
     case '🇦': // team a
       if (matchInformation.players.a.length >= matchInformation.maxTeamCount) {
         reaction.message.channel.send(`${user}, the selected team is full! Please choose a different one.`).then(msg => msg.delete({ timeout: 5000 }))
+        return
       }
       if (playerInformation.valorantRank < matchInformation.rankMinimum || playerInformation.valorantRank > matchInformation.rankMaximum) {
         reaction.message.channel.send(`${user}, you do not meet the match rank requirements! Please try a different one or ask the match creator to adjust them.`).then(msg => msg.delete({ timeout: 5000 }))
@@ -67,12 +70,13 @@ const _addPlayerToMatch = async (reaction, user, GLOBALS, matchInformation) => {
         return
       } else {
         messageEmbed.fields[6].value === 'None' ? messageEmbed.fields[6].value = `• ${playerInformation.valorantUsername}` : messageEmbed.fields[6].value += `\n• ${playerInformation.valorantUsername}`
-        matchInformation.players.a.push(user.id)
-        break
+        matchUpdateQuery.$push['players.a'] = user.id
       }
+      break
     case '🇧': // team b
       if (matchInformation.players.b.length >= matchInformation.maxTeamCount) {
         reaction.message.channel.send(`${user}, the selected team is full! Please choose a different one.`).then(msg => msg.delete({ timeout: 5000 }))
+        return
       }
       if (playerInformation.valorantRank < matchInformation.rankMinimum || playerInformation.valorantRank > matchInformation.rankMaximum) {
         reaction.message.channel.send(`${user}, you do not meet the match rank requirements! Please try a different one or ask the match creator to adjust them.`).then(msg => msg.delete({ timeout: 5000 }))
@@ -80,9 +84,9 @@ const _addPlayerToMatch = async (reaction, user, GLOBALS, matchInformation) => {
         return
       } else {
         messageEmbed.fields[7].value === 'None' ? messageEmbed.fields[7].value = `• ${playerInformation.valorantUsername}` : messageEmbed.fields[7].value += `\n• ${playerInformation.valorantUsername}`
-        matchInformation.players.b.push(user.id)
-        break
+        matchUpdateQuery.$push['players.b'] = user.id
       }
+      break
     case '🇸': // spectators
       if (!matchInformation.spectators) {
         reaction.message.channel.send(`${user}, this match does not allow spectators! Either join a team or ask the match creator to start a new one.`).then(msg => msg.delete({ timeout: 5000 }))
@@ -90,13 +94,13 @@ const _addPlayerToMatch = async (reaction, user, GLOBALS, matchInformation) => {
         return
       } else {
         messageEmbed.fields[8].value === 'None' ? messageEmbed.fields[8].value = `• ${playerInformation.valorantUsername}` : messageEmbed.fields[8].value += `\n• ${playerInformation.valorantUsername}`
-        matchInformation.spectators.push(user.id)
-        break
+        matchUpdateQuery.$push.spectators = user.id
       }
+      break
   }
 
   reaction.message.edit(messageEmbed)
-  return matchInformation
+  return matchUpdateQuery
 }
 
 /**
@@ -108,8 +112,8 @@ const addPlayerToMatch = async (reaction, user, GLOBALS) => {
   const matchInformation = await GLOBALS.mongoDb.collection('matches').findOne({ _id: reaction.message.id })
   if (!matchInformation) return
   if (matchInformation.status !== 'created') return // only pay attention to matches that are still in the creation phase
-  const matchInformationUpdated = await _addPlayerToMatch(reaction, user, GLOBALS, matchInformation)
-  if (matchInformationUpdated) await GLOBALS.mongoDb.collection('matches').replaceOne({ _id: reaction.message.id }, matchInformationUpdated)
+  const matchUpdateQuery = await _addPlayerToMatch(reaction, user, GLOBALS, matchInformation)
+  if (matchUpdateQuery) await GLOBALS.mongoDb.collection('matches').updateOne({ _id: reaction.message.id }, matchUpdateQuery)
 }
 
 /**
@@ -123,25 +127,26 @@ const _removePlayerFromMatch = async (reaction, user, GLOBALS, matchInformation)
 
   const messageEmbed = reaction.message.embeds[0]
 
-  let playersArrayIndex
+  const matchUpdateQuery = { $pullAll: {} }
+
   switch (reaction.emoji.name) {
     case '🇦':
-      playersArrayIndex = matchInformation.players.a.indexOf(user.id)
-      if (playersArrayIndex > -1) matchInformation.players.a.splice(playersArrayIndex, 1)
+      matchUpdateQuery.$pullAll['players.a'] = [user.id]
 
       messageEmbed.fields[6].value = ''
       for (const playerId of matchInformation.players.a) {
+        if (playerId === user.id) continue
         const playerDoc = await GLOBALS.mongoDb.collection('users').findOne({ _id: playerId })
         messageEmbed.fields[6].value += `\n• ${playerDoc.valorantUsername}`
       }
       if (messageEmbed.fields[6].value === '') messageEmbed.fields[6].value = 'None'
       break
     case '🇧':
-      playersArrayIndex = matchInformation.players.b.indexOf(user.id)
-      if (playersArrayIndex > -1) matchInformation.players.b.splice(playersArrayIndex, 1)
+      matchUpdateQuery.$pullAll['players.b'] = [user.id]
 
       messageEmbed.fields[7].value = ''
       for (const playerId of matchInformation.players.b) {
+        if (playerId === user.id) continue
         const playerDoc = await GLOBALS.mongoDb.collection('users').findOne({ _id: playerId })
         messageEmbed.fields[7].value += `\n• ${playerDoc.valorantUsername}`
       }
@@ -149,11 +154,11 @@ const _removePlayerFromMatch = async (reaction, user, GLOBALS, matchInformation)
       break
     case '🇸':
       if (matchInformation.spectators) {
-        playersArrayIndex = matchInformation.spectators.indexOf(user.id)
-        if (playersArrayIndex > -1) matchInformation.spectators.splice(playersArrayIndex, 1)
+        matchUpdateQuery.$pullAll.spectators = [user.id]
 
         messageEmbed.fields[8].value = ''
         for (const playerId of matchInformation.spectators) {
+          if (playerId === user.id) continue
           const playerDoc = await GLOBALS.mongoDb.collection('users').findOne({ _id: playerId })
           messageEmbed.fields[8].value += `\n• ${playerDoc.valorantUsername}`
         }
@@ -164,7 +169,7 @@ const _removePlayerFromMatch = async (reaction, user, GLOBALS, matchInformation)
 
   reaction.users.remove(user.id)
   reaction.message.edit(messageEmbed)
-  return matchInformation
+  return matchUpdateQuery
 }
 
 /**
@@ -176,6 +181,6 @@ const removePlayerFromMatch = async (reaction, user, GLOBALS) => {
   const matchInformation = await GLOBALS.mongoDb.collection('matches').findOne({ _id: reaction.message.id })
   if (!matchInformation) return
   if (matchInformation.status !== 'created') return
-  const matchInformationUpdated = await _removePlayerFromMatch(reaction, user, GLOBALS, matchInformation)
-  if (matchInformationUpdated) await GLOBALS.mongoDb.collection('matches').replaceOne({ _id: reaction.message.id }, matchInformationUpdated)
+  const matchUpdateQuery = await _removePlayerFromMatch(reaction, user, GLOBALS, matchInformation)
+  if (matchUpdateQuery) await GLOBALS.mongoDb.collection('matches').updateOne({ _id: reaction.message.id }, matchUpdateQuery)
 }
