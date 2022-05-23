553e1c072cc302a549e8c229926da821291b7501
Kalissaac <kalissaac@protonmail.com>
2022-05-22 21:26:57 -0700

    Add map thumbnail and rank utils

--- utils/utils.go
+++ utils/utils.go
@@ -7,6 +7,7 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"schwa.tech/scrimbot/database"
 )
 
 func UserIsRegistered(db *mongo.Database, id string) bool {
@@ -45,4 +46,33 @@ func UserIsAdmin(db *mongo.Database, id string) bool {
 func CapitalizeFirstLetter(str string) string {
 	str = strings.ToLower(str)
 	return strings.ToUpper((string)(str[0])) + (string)(str[1:])
+}
+
+func MapThumbnailURL(name string) string {
+	switch strings.ToLower(name) {
+	case "split":
+		return "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt2caea7a88362d6aa/5ecd64b0817e574fa1dcc162/split-minimap-2.png"
+	case "bind":
+		return "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltad4274632c983531/5ecd64d04d187c101f3f2486/bind-minimap-2.png"
+	case "haven":
+		return "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltedb5d57941e4f3f5/5ecd64c14d187c101f3f2484/haven-minimap-2.png"
+	case "ascent":
+		return "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt47bef6aa9e43d8ec/5ecd64df96a8996de38bbf8f/ascent-minimap-2.jpg"
+	case "icebox":
+		return "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt727aeefa1875f8ce/5fc9954afd99385ff600b0f6/Icebox_1a.jpg"
+	case "breeze":
+		return "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt1aa82531c6b3a04b/607fa05b33cf413db790d632/breeze_1a.jpg"
+	case "fracture":
+		return "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt6145fdc7dffa2f5f/6131c5e985514a6ee3fac889/Fracture_Map_Website_Asset.jpg"
+	default:
+		return ""
+	}
+}
+
+func RankNameToID(name string) database.Rank {
+	return 0
+}
+
+func RankIDToName(id database.Rank) string {
+	return "rank"
 }
