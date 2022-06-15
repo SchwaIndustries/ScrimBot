cb13c58dc8521e8b4b950013b10ed5a0e41946cf
Kalissaac <kalissaac@protonmail.com>
2022-06-15 16:47:44 -0700

    Add substring util

--- utils/utils.go
+++ utils/utils.go
@@ -152,3 +152,17 @@ func RankNameToID(name string) database.Rank {
 func RankIDToName(id database.Rank) string {
 	return "rank"
 }
+
+func Substr(input string, start int, length int) string {
+	asRunes := []rune(input)
+
+	if start >= len(asRunes) {
+		return ""
+	}
+
+	if start+length > len(asRunes) {
+		length = len(asRunes) - start
+	}
+
+	return string(asRunes[start : start+length])
+}
